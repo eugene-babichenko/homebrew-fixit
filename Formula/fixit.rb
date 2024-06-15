f973f368f2e40a09dd6df6f2c6e2abe143650004
Yevhenii Babichenko <eugene.babichenko@protonmail.com>
2024-06-15 20:44:38 +0300

    chore: release v0.3.1-beta

--- Formula/fixit.rb
+++ Formula/fixit.rb
@@ -1,22 +1,22 @@
 class Fixit < Formula
   desc "A utility to fix mistakes in your commands."
   homepage "https://github.com/eugene-babichenko/fixit"
-  version "0.3.0-beta"
+  version "0.3.1-beta"
 
   on_macos do
     on_arm do
-      url "https://github.com/eugene-babichenko/fixit/releases/download/v0.3.0-beta/fixit-v0.3.0-beta-aarch64-apple-darwin.tar.gz"
-      sha256 "f35d2ce46468b3a2560de2af6f5f72b3f9259f3300c084f1d57d6e06aadff3f0"
+      url "https://github.com/eugene-babichenko/fixit/releases/download/v0.3.1-beta/fixit-v0.3.1-beta-aarch64-apple-darwin.tar.gz"
+      sha256 "e3603d5ea6b059e9b1706309cd28022b1eb924cc1d2ddf9c60bf445fd7b862cd"
     end
     on_intel do
-      url "https://github.com/eugene-babichenko/fixit/releases/download/v0.3.0-beta/fixit-v0.3.0-beta-x86_64-apple-darwin.tar.gz"
-      sha256 "eb595df867b9e636b24bd190098edba200b0dbcc4a09840029765ac419f47c20"
+      url "https://github.com/eugene-babichenko/fixit/releases/download/v0.3.1-beta/fixit-v0.3.1-beta-x86_64-apple-darwin.tar.gz"
+      sha256 "24ed5c6bed53561372b7df27c77abf2673947fa5f3ae862bea0861432c24e96f"
     end
   end
   on_linux do
     on_intel do
-      url "https://github.com/eugene-babichenko/fixit/releases/download/v0.3.0-beta/fixit-v0.3.0-beta-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "7b2f853b9001a39a47bebc083db988f67c01b05d1d8daaade52aa03ac3bce853"
+      url "https://github.com/eugene-babichenko/fixit/releases/download/v0.3.1-beta/fixit-v0.3.1-beta-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "9da248775cf06b9f5ec24eac348eadd35758e5d69b1f9a15985295a84350093b"
     end
   end
 
